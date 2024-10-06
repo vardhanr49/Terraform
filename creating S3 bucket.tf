6aa9d217798bf4d90dc4658b646f7e54195813a5
vardhanr49 <90019456+vardhanr49@users.noreply.github.com>
2024-10-05 20:20:54 -0500

    Update creating S3 bucket.tf

--- creating S3 bucket.tf
+++ creating S3 bucket.tf
@@ -1,2 +1,12 @@
 #this file contains creation of s3 bucket 
-provider "aws 
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_s3_bucket" "my_bucket" {
+bucket = "my-unique-bucket-name"
+acl = "private"
+tags {
+Name = "My s3 bucket"
+Enivronment = "Dev"
+}
+}

68e11fd9cfe6f5a242dcca7c00295d1f2fdbcc30
vardhanr49 <90019456+vardhanr49@users.noreply.github.com>
2024-10-22 18:59:48 -0500

    Update Route 53. main.tf

--- Route 53. main.tf
+++ Route 53. main.tf
@@ -13,3 +13,19 @@ resource "aws_route_53_zone" "my_zone" {
 output "route53_zone_id" {
   value = aws_route53_zone.my_zone.id
 }
+
+
+# To add DNS records such as an A record pointing to an IP address, extend the file like this:
+# Create an A record in the Route 53 zone
+resource "aws_route53_record" "my_a_record" {
+  zone_id = aws_route53_zone.my_zone.zone_id
+  name    = "www"  # Subdomain (e.g., www.example.com)
+  type    = "A"
+  ttl     = 300
+  records = ["12.34.56.78"]  # Replace with your IP address
+
+  tags = {
+    Name = "my-a-record"
+  }
+}
+
